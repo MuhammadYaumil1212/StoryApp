6d211bb6f8de1cb78f289490d3204c49c24d0149
Muhammad Yaumil Ramadhani <muhammadyaumil1313@gmail.com>
2023-06-19 15:38:05 +0700

    fix bug logout button

--- app/src/main/java/org/d3if00001/storyapp/presentations/ui/AddStory.kt
+++ app/src/main/java/org/d3if00001/storyapp/presentations/ui/AddStory.kt
@@ -14,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
